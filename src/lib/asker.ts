9cb04eeadc7efb40d5dcd06836852db671892b77
Piyush Acharya <acharyapiyush1@outlook.com>
2022-10-16 16:09:30 -0700

    Tested Fleet's autoformatter

--- src/lib/asker.ts
+++ src/lib/asker.ts
@@ -1,114 +1,116 @@
-import { writable } from "svelte/store";
-import { HouseholdType, questions, QuestionType, type MultipleChoiceQuestion, type NumberQuestion } from "./questions";
+import {writable} from "svelte/store";
+import {HouseholdType, questions, QuestionType, type MultipleChoiceQuestion, type NumberQuestion} from "./questions";
 
 let $values: any[] = [];
 for (let q of questions) {
-  switch (q.type) {
-    case QuestionType.MultipleChoice:
-      $values.push(Object.values((q.data as MultipleChoiceQuestion<any>).choices)[0]);
-      break;
+    switch (q.type) {
+        case QuestionType.MultipleChoice:
+            $values.push(Object.values((q.data as MultipleChoiceQuestion<any>).choices)[0]);
+            break;
 
-    case QuestionType.Number:
-      $values.push((q.data as NumberQuestion).default);
-      break;
-  }
+        case QuestionType.Number:
+            $values.push((q.data as NumberQuestion).default);
+            break;
+    }
 }
 
 export let values = writable($values);
-values.subscribe(v => { $values = v });
+values.subscribe(v => {
+    $values = v
+});
 
 export function calculate(): number {
-  let res = 0;
+    let res = 0;
 
-  // Shower
-  let mult = 0;
-  switch ($values[0]) {
-    case HouseholdType.Old:
-      mult = 5;
-      break;
+    // Shower
+    let mult = 0;
+    switch ($values[0]) {
+        case HouseholdType.Old:
+            mult = 5;
+            break;
 
-    case HouseholdType.Standard:
-      mult = 3.8;
-      break;
+        case HouseholdType.Standard:
+            mult = 3.8;
+            break;
 
-    case HouseholdType.Efficient:
-      mult = 2.5;
-      break;
-  }
-  res += mult * $values[2] * $values[1];
+        case HouseholdType.Efficient:
+            mult = 2.5;
+            break;
+    }
+    res += mult * $values[2] * $values[1];
 
-  // Baths
-  res += 35 * $values[3] * $values[1]; // Average bath takes 35 gallons
+    // Baths
+    res += 35 * $values[3] * $values[1]; // Average bath takes 35 gallons
 
-  // Sinks
-  switch ($values[0]) {
-    case HouseholdType.Old:
-      mult = 5;
-      break;
+    // Sinks
+    switch ($values[0]) {
+        case HouseholdType.Old:
+            mult = 5;
+            break;
 
-    case HouseholdType.Standard:
-      mult = 3.3
-      break;
+        case HouseholdType.Standard:
+            mult = 3.3
+            break;
 
-    case HouseholdType.Efficient:
-      mult = 1.5;
-      break;
-  }
-  res += $values[4] * mult * $values[1];
+        case HouseholdType.Efficient:
+            mult = 1.5;
+            break;
+    }
+    res += $values[4] * mult * $values[1];
 
-  // Toilets
-  switch ($values[0]) {
-    case HouseholdType.Old:
-      mult = 5;
-      break;
+    // Toilets
+    switch ($values[0]) {
+        case HouseholdType.Old:
+            mult = 5;
+            break;
 
-    case HouseholdType.Standard:
-      mult = 3.3
-      break;
+        case HouseholdType.Standard:
+            mult = 3.3
+            break;
 
-    case HouseholdType.Efficient:
-      mult = 1.6;
-      break;
-  }
-  res += 5 * mult * $values[1]; // Average person flushes 5x a day
+        case HouseholdType.Efficient:
+            mult = 1.6;
+            break;
+    }
+    res += 5 * mult * $values[1]; // Average person flushes 5x a day
 
-  // Kitchen sink
-  if ($values[0] === HouseholdType.Efficient) {
-    mult = 1.5;
-  } else {
-    mult = 5;
-  }
-  res += $values[5] * mult * $values[1];
+    // Kitchen sink
+    if ($values[0] === HouseholdType.Efficient) {
+        mult = 1.5;
+    } else {
+        mult = 5;
+    }
+    res += $values[5] * mult * $values[1];
 
-  // Dishes
-  if ($values[0] === HouseholdType.Efficient) {
-    mult = 4.3;
-  } else {
-    mult = 15;
-  }
-  if ($values[7]) {
-    mult = 27;
-  }
-  res += $values[6] * mult;
+    // Dishes
+    if ($values[0] === HouseholdType.Efficient) {
+        mult = 4.3;
+    } else {
+        mult = 15;
+    }
+    if ($values[7]) {
+        mult = 27;
+    }
+    res += $values[6] * mult;
 
-  // Laundry
-  if ($values[0] === HouseholdType.Efficient) {
-    mult = 20;
-  } else {
-    mult = 41;
-  }
-  res += $values[8] * mult;
+    // Laundry
+    if ($values[0] === HouseholdType.Efficient) {
+        mult = 20;
+    } else {
+        mult = 41;
+    }
+    res += $values[8] * mult;
 
-  // Lawn
-  res += $values[9] * $values[10];
+    // Lawn
+    res += $values[9] * $values[10];
 
-  // Swimming pool
-  if ($values[11]) {
-    res += (18000 + $values[12]) / 365;
-  }
+    // Swimming pool
+    if ($values[11]) {
+        res += (18000 + $values[12]) / 365;
+    }
 
-  // Car
-  res += $values[13] * $values[14];
+    // Car
+    res += $values[13] * $values[14];
 
-  return Math.round(res);
+    return Math.round(res);
 }
--- src/lib/questions.ts
+++ src/lib/questions.ts
@@ -1,246 +1,246 @@
 export interface Question<T> {
-  title: string,
-  type: QuestionType,
-  data: MultipleChoiceQuestion<T> | NumberQuestion,
-  condition?: Condition,
+    title: string,
+    type: QuestionType,
+    data: MultipleChoiceQuestion<T> | NumberQuestion,
+    condition?: Condition,
 }
 
 export enum QuestionType {
-  MultipleChoice,
-  Number,
+    MultipleChoice,
+    Number,
 }
 
 export interface MultipleChoiceQuestion<T> {
-  choices: Record<string, T>,
+    choices: Record<string, T>,
 }
 
 export interface NumberQuestion {
-  min: number,
-  max?: number,
-  default: number,
+    min: number,
+    max?: number,
+    default: number,
 }
 
 export interface Condition {
-  offset: number,
-  check: (v: any) => boolean,
+    offset: number,
+    check: (v: any) => boolean,
 }
 
 // Questions
 export enum HouseholdType {
-  Old,
-  Standard,
-  Efficient
+    Old,
+    Standard,
+    Efficient
 }
 
 export const questions: Question<any>[] = [
-  {
-    title: "What type of household do you have?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Pre-1980": HouseholdType.Old,
-        "Standard": HouseholdType.Standard,
-        "Efficient (Energy Star appliances, low-flow showerheads, etc.)": HouseholdType.Efficient,
-      }
+    {
+        title: "What type of household do you have?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Pre-1980": HouseholdType.Old,
+                "Standard": HouseholdType.Standard,
+                "Efficient (Energy Star appliances, low-flow showerheads, etc.)": HouseholdType.Efficient,
+            }
+        },
     },
-  },
-  {
-    title: "How many people live in your household?",
-    type: QuestionType.Number,
-    data: {
-      min: 1,
-      default: 1,
+    {
+        title: "How many people live in your household?",
+        type: QuestionType.Number,
+        data: {
+            min: 1,
+            default: 1,
+        },
     },
-  },
-  {
-    title: "How long is the average shower in your household?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Under 5 minutes": 4,
-        "5-10 minutes": 8,
-        "11-15 minutes": 13,
-        "Over 15 minutes": 15,
-      }
+    {
+        title: "How long is the average shower in your household?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Under 5 minutes": 4,
+                "5-10 minutes": 8,
+                "11-15 minutes": 13,
+                "Over 15 minutes": 15,
+            }
+        },
     },
-  },
-  {
-    title: "How often do people in your household take baths?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Never": 0,
-        "Once a year": 1 / 365,
-        "Once a month": 1 / 30,
-        "Once a week": 1 / 7,
-        "Once a day": 1,
-      }
+    {
+        title: "How often do people in your household take baths?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Never": 0,
+                "Once a year": 1 / 365,
+                "Once a month": 1 / 30,
+                "Once a week": 1 / 7,
+                "Once a day": 1,
+            }
+        }
+    },
+    {
+        title: "How long do people in your household leave the bathroom faucets running?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Under 4 minutes": 4,
+                "4-10 minutes": 8,
+                "11-30 minutes": 20,
+                "Over 30 minutes": 30,
+            }
+        },
+    },
+    {
+        title: "Do you let it 'mellow' in the toilet?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Of course!": 1 / 4,
+                "Gross, no!": 1,
+                "Sometimes": 1 / 2,
+            }
+        },
+    },
+    {
+        title: "How long do members of your household leave the kitchen sink running (not including washing dishes)?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Under 5 minutes": 4,
+                "5-20 minutes": 13,
+                "21-45 minutes": 33,
+                "Over 45 minutes": 45,
+            }
+        }
+    },
+    {
+        title: "How often does your household wash dishes?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Disposable dishes": 0,
+                "Once a week": 1 / 7,
+                "Once a day": 1,
+                "Twice a day": 2,
+            }
+        }
+    },
+    {
+        title: "How are dishes washed?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "By hand": true,
+                "Dishwasher": false,
+                // TODO "Trash 'em or Eat Out": false,
+            },
+        },
+        condition: {
+            offset: -1,
+            check: (v: number) => v != 0,
+        },
+    },
+    {
+        title: "How often does your household do laundry?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Never": 0,
+                "Once a month": 1 / 30,
+                "Once a week": 1 / 7,
+                "Once a day": 1,
+            }
+        }
+    },
+    {
+        title: "How often do you water your lawn?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "Never": 0,
+                "Once a month": 1 / 30,
+                "Once a week": 1 / 7,
+                "Once a day": 1,
+            },
+        },
+    },
+    {
+        title: "How large is the are of your lawn you water?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "1-99 square feet": 17,
+                "100-500 square feet": 99,
+                "500-1,000 square feet": 248,
+                "1,000-5,000 square feet": 990,
+                "5,000-10,000 square feet": 2470,
+                "10,000-40,000 square feet": 8250,
+                "Over 40,000 square feet": 14375,
+            },
+        },
+        condition: {
+            offset: -1,
+            check: (v: number) => v != 0,
+        },
+    },
+    {
+        title: "Do you have a swimming pool?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "No": false,
+                "Yes": true,
+            },
+        },
+    },
+    {
+        title: "How long do you leave the pool uncovered?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "1": 1000,
+                "2": 2000,
+                "3": 3000,
+                "4": 4000,
+                "5": 5000,
+                "6": 6000,
+                "7": 7000,
+                "8": 8000,
+                "9": 9000,
+                "10": 10000,
+                "11": 11000,
+                "12": 12000,
+            },
+        },
+        condition: {
+            offset: -1,
+            check: (v: boolean) => v,
+        },
+    },
+    {
+        title: "How often do you wash your car?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "I don't have a car": 0,
+                "Once a year": 1 / 365,
+                "Once a month": 1 / 30,
+                "Once a week": 1 / 7,
+                "Once a day": 1,
+            },
+        },
+    },
+    {
+        title: "How do you wash your car?",
+        type: QuestionType.MultipleChoice,
+        data: {
+            choices: {
+                "At home": 100,
+                "Car wash": 58,
+            },
+        },
+        condition: {
+            offset: -1,
+            check: (v: number) => v != 0,
+        },
     }
-  },
-  {
-    title: "How long do people in your household leave the bathroom faucets running?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Under 4 minutes": 4,
-        "4-10 minutes": 8,
-        "11-30 minutes": 20,
-        "Over 30 minutes": 30,
-      }
-    },
-  },
-  {
-    title: "Do you let it 'mellow' in the toilet?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Of course!": 1 / 4,
-        "Gross, no!": 1,
-        "Sometimes": 1 / 2,
-      }
-    },
-  },
-  {
-    title: "How long do members of your household leave the kitchen sink running (not including washing dishes)?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Under 5 minutes": 4,
-        "5-20 minutes": 13,
-        "21-45 minutes": 33,
-        "Over 45 minutes": 45,
-      }
-    }
-  },
-  {
-    title: "How often does your household wash dishes?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Disposable dishes": 0,
-        "Once a week": 1 / 7,
-        "Once a day": 1,
-        "Twice a day": 2,
-      }
-    }
-  },
-  {
-    title: "How are dishes washed?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "By hand": true,
-        "Dishwasher": false,
-        // TODO "Laundromat": false,
-      },
-    },
-    condition: {
-      offset: -1,
-      check: (v: number) => v != 0,
-    },
-  },
-  {
-    title: "How often does your household do laundry?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Never": 0,
-        "Once a month": 1 / 30,
-        "Once a week": 1 / 7,
-        "Once a day": 1,
-      }
-    }
-  },
-  {
-    title: "How often do you water your lawn?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "Never": 0,
-        "Once a month": 1 / 30,
-        "Once a week": 1 / 7,
-        "Once a day": 1,
-      },
-    },
-  },
-  {
-    title: "How large is the are of your lawn you water?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "1-99 square feet": 17,
-        "100-500 square feet": 99,
-        "500-1,000 square feet": 248,
-        "1,000-5,000 square feet": 990,
-        "5,000-10,000 square feet": 2470,
-        "10,000-40,000 square feet": 8250,
-        "Over 40,000 square feet": 14375,
-      },
-    },
-    condition: {
-      offset: -1,
-      check: (v: number) => v != 0,
-    },
-  },
-  {
-    title: "Do you have a swimming pool?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "No": false,
-        "Yes": true,
-      },
-    },
-  },
-  {
-    title: "How long do you leave the pool uncovered?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "1": 1000,
-        "2": 2000,
-        "3": 3000,
-        "4": 4000,
-        "5": 5000,
-        "6": 6000,
-        "7": 7000,
-        "8": 8000,
-        "9": 9000,
-        "10": 10000,
-        "11": 11000,
-        "12": 12000,
-      },
-    },
-    condition: {
-      offset: -1,
-      check: (v: boolean) => v,
-    },
-  },
-  {
-    title: "How often do you wash your car?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "I don't have a car": 0,
-        "Once a year": 1 / 365,
-        "Once a month": 1 / 30,
-        "Once a week": 1 / 7,
-        "Once a day": 1,
-      },
-    },
-  },
-  {
-    title: "How do you wash your car?",
-    type: QuestionType.MultipleChoice,
-    data: {
-      choices: {
-        "At home": 100,
-        "Car wash": 58,
-      },
-    },
-    condition: {
-      offset: -1,
-      check: (v: number) => v != 0,
-    },
-  }
 ];
