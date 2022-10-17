c94584f8afb26844aacc007eb571dac17bb8110a
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-10-17 18:03:27 +0000

    Smh I already knew this

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -5,9 +5,6 @@
 <div class="about-section">
   <h1>About Us Page</h1>
   <p>Some text about who we are and what we do.</p>
-  <p>
-    Resize the browser window to see that this page is responsive by the way.
-  </p>
 </div>
 
 <h2 style="text-align:center">Our Team</h2>
