66b299b287bf8d8186d753b4ada227ab6563964c
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-10-17 18:02:09 +0000

    Fix cards

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,39 +14,36 @@
 <div class="row">
   <div class="col">
     <div class="card">
-      <img src="/w3images/team1.jpg" alt="Test" style="width:100%" />
-      <div class="container">
-        <h2>Aarnav Bhat</h2>
-        <p class="title">Researcher</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>jane@example.com</p>
-        <p><button class="button">Contact</button></p>
+      <img src="/w3images/team1.jpg" alt="Test" style="width:100%" class="card-img-top"/>
+      <div class="card-body">
+        <h5 class="card-title">Aarnav Bhat</h5>
+        <p class="card-text">Researcher</p>
+        <p class="card-text">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
       </div>
     </div>
   </div>
 
   <div class="col">
     <div class="card">
-      <img src="/profile/Piyush.png" alt="Piyush" style="width:100%" />
-      <div class="container">
-        <h2>Piyush Acharya</h2>
-        <p class="title">Researcher + Website Developer</p>
-        <p>Piyush Acharya is an freshman at Interlake High School interested in SWE and ML. He recently placed first at the U.S. Department of Energy's National Science Bowl Finals. Piyush is highly passionate about exploring different ways in which he can advance his skills in the branches of science while also tying it with his passion for computer science. He created and is the lead developer at DNAnalyzer, an open source project for analyzing DNA sequences where he manages a team of over 20 developers.</p>
-        <p>contact@piyushacharya.com</p>
-        <p><button href="www.piyushacharya.com" class="button">Contact</button></p>
+      <img src="/profile/Piyush.png" alt="Piyush" style="width:100%" class="card-img-top"/>
+      <div class="card-body">
+        <h5 class="card-title">Piyush Acharya</h5>
+        <p class="card-text">Researcher + Website Developer</p>
+        <p class="card-text">Piyush Acharya is an freshman at Interlake High School interested in SWE and ML. He recently placed first at the U.S. Department of Energy's National Science Bowl Finals. Piyush is highly passionate about exploring different ways in which he can advance his skills in the branches of science while also tying it with his passion for computer science. He created and is the lead developer at DNAnalyzer, an open source project for analyzing DNA sequences where he manages a team of over 20 developers.</p>
+        <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
       </div>
     </div>
   </div>
 
   <div class="col">
     <div class="card">
-      <img src="/w3images/team3.jpg" alt="John" style="width:100%" />
-      <div class="container">
-        <h2>Nishant Vikramaditya</h2>
-        <p class="title">Website Developer</p>
-        <p>Some text that describes me lorem ipsum ipsum lorem.</p>
-        <p>john@example.com</p>
-        <p><button class="button">Contact</button></p>
+      <img src="/w3images/team3.jpg" alt="John" style="width:100%" class="card-img-top"/>
+      <div class="card-body">
+        <h5 class="card-title">Nishant Vikramaditya</h5>
+        <p class="card-text">Website Developer</p>
+        <p class="card-text">Some text that describes me lorem ipsum ipsum lorem.</p>
+        <a href="https://nv7haven.com" class="btn btn-primary">Contact</a>
       </div>
     </div>
   </div>
