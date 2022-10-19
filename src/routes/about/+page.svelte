de6cb0f4be4920e97c3c54cb8f5c675de8829b70
Aarnav Bhat <aarnavpbhat@gmail.com>
2022-10-18 17:27:53 -0700

    Added details to result page

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -11,10 +11,10 @@
 <div class="row">
   <div class="col">
     <div class="card">
-      <img src="/w3images/team1.jpg" alt="Test" style="width:100%" class="card-img-top"/>
+      <img src="/w3images/team1.jpg" alt="Aarnav" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Aarnav Bhat</h5>
-        <p class="card-text">Researcher</p>
+        <p class="card-text">Aarnav is one our researchers who also helps design our website. </p>
         <p class="card-text">Some text that describes me lorem ipsum ipsum lorem.</p>
         <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
       </div>
@@ -26,7 +26,7 @@
       <img src="/profile/Piyush.png" alt="Piyush" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Piyush Acharya</h5>
-        <p class="card-text">Researcher + Website Developer</p>
+        <p class="card-text">Piyush is one of our researchers who also helps develop our website.</p>
         <p class="card-text">Piyush Acharya is an freshman at Interlake High School interested in SWE and ML. He recently placed first at the U.S. Department of Energy's National Science Bowl Finals. Piyush is highly passionate about exploring different ways in which he can advance his skills in the branches of science while also tying it with his passion for computer science. He created and is the lead developer at DNAnalyzer, an open source project for analyzing DNA sequences where he manages a team of over 20 developers.</p>
         <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
       </div>
@@ -35,10 +35,10 @@
 
   <div class="col">
     <div class="card">
-      <img src="/w3images/team3.jpg" alt="John" style="width:100%" class="card-img-top"/>
+      <img src="/w3images/team3.jpg" alt="Nishant" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Nishant Vikramaditya</h5>
-        <p class="card-text">Website Developer</p>
+        <p class="card-text">Nishant is our main Website Developer and Designer.</p>
         <p class="card-text">Some text that describes me lorem ipsum ipsum lorem.</p>
         <a href="https://nv7haven.com" class="btn btn-primary">Contact</a>
       </div>
