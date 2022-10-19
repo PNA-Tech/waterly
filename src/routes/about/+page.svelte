8c864a828b068d70c5c2f7362ebb74e418a12aaa
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-10-19 00:31:33 +0000

    Update roles

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -14,7 +14,7 @@
       <img src="/w3images/team1.jpg" alt="Aarnav" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Aarnav Bhat</h5>
-        <p class="card-text">Aarnav is one our researchers who also helps design our website. </p>
+        <p class="card-text">Researcher</p>
         <p class="card-text">Some text that describes me lorem ipsum ipsum lorem.</p>
         <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
       </div>
@@ -26,7 +26,7 @@
       <img src="/profile/Piyush.png" alt="Piyush" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Piyush Acharya</h5>
-        <p class="card-text">Piyush is one of our researchers who also helps develop our website.</p>
+        <p class="card-text">Researcher</p>
         <p class="card-text">Piyush Acharya is an freshman at Interlake High School interested in SWE and ML. He recently placed first at the U.S. Department of Energy's National Science Bowl Finals. Piyush is highly passionate about exploring different ways in which he can advance his skills in the branches of science while also tying it with his passion for computer science. He created and is the lead developer at DNAnalyzer, an open source project for analyzing DNA sequences where he manages a team of over 20 developers.</p>
         <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
       </div>
@@ -38,7 +38,7 @@
       <img src="/w3images/team3.jpg" alt="Nishant" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Nishant Vikramaditya</h5>
-        <p class="card-text">Nishant is our main Website Developer and Designer.</p>
+        <p class="card-text">Lead Developer</p>
         <p class="card-text">Some text that describes me lorem ipsum ipsum lorem.</p>
         <a href="https://nv7haven.com" class="btn btn-primary">Contact</a>
       </div>
