e3a6454eadf4fed7581d71eb0c51a27c713a28dc
Piyush Acharya <acharyapiyush1@outlook.com>
2022-10-29 15:20:17 -0700

    Added Waterly Description

--- src/routes/about/+page.svelte
+++ src/routes/about/+page.svelte
@@ -3,8 +3,20 @@
 </svelte:head>
 
 <div class="about-section">
-  <h1>About Us Page</h1>
-  <p>Some text about who we are and what we do.</p>
+  <h1>About Us</h1>
+  <p>
+    Waterly is a water calculator that asks questions to determine a person's
+    water use in gallons per year and also estimates your bill based on your
+    residence. Based on your responses to the questions, our website sends you
+    personalized tips and suggests the following steps to help you create a more
+    sustainable environment. Our customized recommendations provide estimations
+    so that the user can learn more and be more motivated by listing the
+    benefits of following our customized tips. Once they finish the questions,
+    credible resources are provided to assist the user in gaining more knowledge
+    and becoming more aware of their surroundings, as well as to assist the user
+    in learning about their water footprint and the small steps they can take to
+    learn more.
+  </p>
 </div>
 
 <h2 style="text-align:center">Our Team</h2>
@@ -15,20 +27,46 @@
       <div class="card-body">
         <h5 class="card-title">Aarnav Bhat</h5>
         <p class="card-text">Researcher</p>
-        <p class="card-text">Aarnav Bhat is a freshman at Interlake High School. He is passionate about Computer Science and Business and helps his local community by refereeing games and tutoring kids via Schoohouse.world.  </p>
-        <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
+        <p class="card-text">
+          Aarnav Bhat is a freshman at Interlake High School. He is passionate
+          about Computer Science and Business and helps his local community by
+          refereeing games and tutoring kids via Schoohouse.world.
+        </p>
+        <a
+          href="https://www.piyushacharya.com"
+          target="_blank"
+          class="btn btn-primary">Contact</a
+        >
       </div>
     </div>
   </div>
 
   <div class="col">
     <div class="card">
-      <img src="/profile/Piyush.png" alt="Piyush" style="width:100%" class="card-img-top"/>
       <div class="card-body">
         <h5 class="card-title">Piyush Acharya</h5>
         <p class="card-text">Researcher</p>
-        <p class="card-text">Piyush Acharya is a freshman at Interlake High School interested in SWE and ML. He recently placed first at the U.S. Department of Energy's National Science Bowl Finals. Piyush is highly passionate about exploring different ways in which he can advance his skills in the branches of science while also tying it with his passion for computer science. He created and is the lead developer at DNAnalyzer, an open source project for analyzing DNA sequences where he manages a team of over 20 developers.</p>
-        <a href="https://www.piyushacharya.com" class="btn btn-primary">Contact</a>
+        <p class="card-text">
+          Piyush Acharya is a freshman at Interlake High School interested in
+          SWE and ML. He recently placed first at the U.S. Department of
+          Energy's <a
+            href="https://www.energy.gov/articles/doe-announces-winners-32nd-annual-national-science-bowl#:~:text=include%3A%20Vishnu%20Mangipudi%2C-,Piyush%20Acharya,-%2C%20Aishwarya%20Agrawal%2C%20Aryan"
+            target="_blank">National Science Bowl Finals</a
+          >. Piyush is highly passionate about exploring different ways in which
+          he can advance his skills in the branches of science while also tying
+          it with his passion for computer science. He founded and is the lead
+          developer of
+          <a
+            href="https://github.com/Verisimilitude11/DNAnalyzer"
+            target="_blank">DNAnalyzer</a
+          >, an open source project that analyzes DNA sequences where he manages
+          a team of over 30 developers.
+        </p>
+        <a
+          href="https://www.piyushacharya.com"
+          target="_blank"
+          class="btn btn-primary">Contact</a
+        >
       </div>
     </div>
   </div>
@@ -39,7 +77,17 @@
       <div class="card-body">
         <h5 class="card-title">Nishant Vikramaditya</h5>
         <p class="card-text">Lead Developer</p>
-        <p class="card-text">Nishant Vikramaditya is a freshman at Interlake High School. He is a passionate developer who enjoys backend development and database architecture. On his <a href="https://nv7haven.com" target="_blank">website</a>, he has a variety of games and projects showcasing his interest in programming. He is also the lead developer for Elemental on Discord, which handles over 500 commands per day. In his spare time, Nishant enjoys developing Discord bots and experimenting with various programming languages.</p>
+        <p class="card-text">
+          Nishant Vikramaditya is a freshman at Interlake High School. He is a
+          passionate developer who enjoys backend development and database
+          architecture. On his <a href="https://nv7haven.com" target="_blank"
+            >website</a
+          >, he has a variety of games and projects showcasing his interest in
+          programming. He is also the lead developer for Elemental on Discord,
+          which handles over 500 commands per day. In his spare time, Nishant
+          enjoys developing Discord bots and experimenting with various
+          programming languages.
+        </p>
         <a href="https://nv7haven.com" class="btn btn-primary">Contact</a>
       </div>
     </div>
