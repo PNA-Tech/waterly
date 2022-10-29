d1eb626ef2818ccaa02e6864019ce337f1f798e5
Aarnav Bhat <aarnavpbhat@gmail.com>
2022-10-29 12:58:05 -0700

    Added text

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -44,7 +44,7 @@
             <img src="https://bellevuewa.gov/sites/default/files/styles/max_650x650/public/inline-images/2020-11/2021-2022%20typical%20bill%20graphic%20horizontal.PNG?itok=JgZqfxj9" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">Water Bills by State</h5>
-            <p class="card-text">Lorem ipsum</p>
+            <p class="card-text">How what state you live in affects your water bill.</p>
             <a href="https://dripfina.com/water-bill-by-us-states/#:~:text=What%27s%20The%20Average%20Monthly%20Water%20Bill%20in%20the,7%20Colorado%20...%208%20Connecticut%20...%20More%20items" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
@@ -55,7 +55,7 @@
             <img src="https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/DO-trend.png?itok=xIMjHmdv" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">Domestic Water Usage Survey</h5>
-            <p class="card-text">Lorem ipsum</p>
+            <p class="card-text">Learn how water use changed over 65 years.</p>
             <a href="https://www.usgs.gov/mission-areas/water-resources/science/domestic-water-use" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
@@ -66,7 +66,7 @@
             <img src="https://www.conservenorthtexas.org/getmedia/0e6eb510-27fa-441c-8358-0e1547b85a87/EPA_USgpcd.png" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">Domestic Water Usage Growth</h5>
-            <p class="card-text">Lorem ipsum</p>
+            <p class="card-text">Learn about the project change in Water Use for 2030.</p>
             <a href="https://www.conservenorthtexas.org/regional-indicators/us-domestic-water-use-gallons-day-person" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
