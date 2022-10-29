efedf4b27b6a58ad18c8fec6e725d01e5b7c7f33
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2022-10-29 18:55:41 +0000

    Remove external resources

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -5,45 +5,70 @@
 <h1>Resources</h1>
 <p class="lead">Learn more about how water footprint, how where you live affects you, and what you can do the help the Earth.</p>
 
-<div class="row row-cols-1 row-cols-md-4 g-4 justify-content-center">
-<div class="col">
-    <div class="card">
-    <img src="https://www.epa.gov/sites/default/files/2017-02/ws-water-understandingbill.jpg" class="card-img-top" alt="Water Bill Cover"/>
-    <div class="card-body">
-        <h5 class="card-title">Water Bill</h5>
-        <p class="card-text">An in-depth explanation of your water bill.</p>
-        <a href="https://www.epa.gov/watersense/understanding-your-water-bill" target="_blank" class="btn btn-primary">Learn more</a>
-    </div>
-    </div>
-</div>
-
-<div class="col">
-    <div class="card">
-    <img src="https://www.epa.gov/sites/default/files/2017-02/ws-water-howeusewater.jpg" class="card-img-top" alt="Water Bill Cover"/>
-    <div class="card-body">
-        <h5 class="card-title">How we use Water</h5>
-        <p class="card-text">How you use water in your everyday life.</p>
-        <a href="https://www.epa.gov/watersense/how-we-use-water" target="_blank" class="btn btn-primary">Learn more</a>
-    </div>
-    </div>
-</div>
-
-<div class="col">
-    <div class="card">
-        <img src="https://eamenergy.com/wp-content/uploads/2013/07/header_water-1024x403.jpg" class="card-img-top" alt="Water Bill Cover"/>
+<div class="row row-cols-1 row-cols-md-3 g-4 justify-content-center">
+    <div class="col">
+        <div class="card">
+        <img src="https://www.epa.gov/sites/default/files/2017-02/ws-water-understandingbill.jpg" class="card-img-top" alt="Water Bill Cover"/>
         <div class="card-body">
-        <h5 class="card-title">WaterSense Products</h5>
-        <p class="card-text">Find approved WaterSense products you can use.</p>
-        <a href="https://lookforwatersense.epa.gov/products/" target="_blank" class="btn btn-primary">Learn more</a>
+            <h5 class="card-title">Water Bill</h5>
+            <p class="card-text">An in-depth explanation of your water bill.</p>
+            <a href="https://www.epa.gov/watersense/understanding-your-water-bill" target="_blank" class="btn btn-primary">Learn more</a>
+        </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+        <img src="https://www.epa.gov/sites/default/files/2017-02/ws-water-howeusewater.jpg" class="card-img-top" alt="Water Bill Cover"/>
+        <div class="card-body">
+            <h5 class="card-title">How we use Water</h5>
+            <p class="card-text">How you use water in your everyday life.</p>
+            <a href="https://www.epa.gov/watersense/how-we-use-water" target="_blank" class="btn btn-primary">Learn more</a>
+        </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+            <img src="https://eamenergy.com/wp-content/uploads/2013/07/header_water-1024x403.jpg" class="card-img-top" alt="Water Bill Cover"/>
+            <div class="card-body">
+            <h5 class="card-title">WaterSense Products</h5>
+            <p class="card-text">Find approved WaterSense products you can use.</p>
+            <a href="https://lookforwatersense.epa.gov/products/" target="_blank" class="btn btn-primary">Learn more</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+            <img src="https://bellevuewa.gov/sites/default/files/styles/max_650x650/public/inline-images/2020-11/2021-2022%20typical%20bill%20graphic%20horizontal.PNG?itok=JgZqfxj9" class="card-img-top" alt="Water Bill Cover"/>
+            <div class="card-body">
+            <h5 class="card-title">Water Bills by State</h5>
+            <p class="card-text">Lorem ipsum</p>
+            <a href="https://dripfina.com/water-bill-by-us-states/#:~:text=What%27s%20The%20Average%20Monthly%20Water%20Bill%20in%20the,7%20Colorado%20...%208%20Connecticut%20...%20More%20items" target="_blank" class="btn btn-primary">Learn more</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+            <img src="https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/DO-trend.png?itok=xIMjHmdv" class="card-img-top" alt="Water Bill Cover"/>
+            <div class="card-body">
+            <h5 class="card-title">Domestic Water Usage Survey</h5>
+            <p class="card-text">Lorem ipsum</p>
+            <a href="https://www.usgs.gov/mission-areas/water-resources/science/domestic-water-use" target="_blank" class="btn btn-primary">Learn more</a>
+            </div>
+        </div>
+    </div>
+
+    <div class="col">
+        <div class="card">
+            <img src="https://www.conservenorthtexas.org/getmedia/0e6eb510-27fa-441c-8358-0e1547b85a87/EPA_USgpcd.png" class="card-img-top" alt="Water Bill Cover"/>
+            <div class="card-body">
+            <h5 class="card-title">Domestic Water Usage Growth</h5>
+            <p class="card-text">Lorem ipsum</p>
+            <a href="https://www.conservenorthtexas.org/regional-indicators/us-domestic-water-use-gallons-day-person" target="_blank" class="btn btn-primary">Learn more</a>
+            </div>
         </div>
     </div>
 </div>
-</div>
-
-<h3>External Resources</h3>
-<p class="lead">Some resources we used to make this website!</p>
-<ul class="list-group mb-3">
-    <a class="list-group-item" target="_blank" href="https://dripfina.com/water-bill-by-us-states/#:~:text=What%27s%20The%20Average%20Monthly%20Water%20Bill%20in%20the,7%20Colorado%20...%208%20Connecticut%20...%20More%20items">Water Bills by State</a>
-    <a class="list-group-item" target="_blank" href="https://www.usgs.gov/mission-areas/water-resources/science/domestic-water-use">Domestic Water Usage Survey</a>
-    <a class="list-group-item" target="_blank" href="https://www.conservenorthtexas.org/regional-indicators/us-domestic-water-use-gallons-day-person">Domestic Water Usage Growth</a>
-</ul>
