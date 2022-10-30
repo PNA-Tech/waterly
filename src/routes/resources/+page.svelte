35f499b04897863492ecd695a17bf2e3ead88d70
Piyush Acharya <acharyapiyush1@outlook.com>
2022-10-29 18:16:05 -0700

    Added description

--- src/routes/resources/+page.svelte
+++ src/routes/resources/+page.svelte
@@ -11,7 +11,7 @@
         <img src="https://www.epa.gov/sites/default/files/2017-02/ws-water-understandingbill.jpg" class="card-img-top" alt="Water Bill Cover"/>
         <div class="card-body">
             <h5 class="card-title">Water Bill</h5>
-            <p class="card-text">An in-depth explanation of your water bill.</p>
+            <p class="card-text">Understanding how much water you use now is the first step in changing the way you use water in the future. This information is best found on your monthly water bill. Take your water bill and follow the instructions below to learn more about it and your individual water consumption.</p>
             <a href="https://www.epa.gov/watersense/understanding-your-water-bill" target="_blank" class="btn btn-primary">Learn more</a>
         </div>
         </div>
@@ -21,8 +21,8 @@
         <div class="card">
         <img src="https://www.epa.gov/sites/default/files/2017-02/ws-water-howeusewater.jpg" class="card-img-top" alt="Water Bill Cover"/>
         <div class="card-body">
-            <h5 class="card-title">How we use Water</h5>
-            <p class="card-text">How you use water in your everyday life.</p>
+            <h5 class="card-title">How We Use Water</h5>
+            <p class="card-text">Although the Earth appears to have an abundance of water, only around 1% of it is available for human use. The remainder is either saline water from seas, fresh water trapped in polar ice caps, or too inaccessible for practical use. Despite rising population and demand for freshwater resources, availability will always be steady. And, while the water cycle regularly returns water to Earth, it is not always restored in the same area, amount, or quality.</p>
             <a href="https://www.epa.gov/watersense/how-we-use-water" target="_blank" class="btn btn-primary">Learn more</a>
         </div>
         </div>
@@ -33,7 +33,7 @@
             <img src="https://eamenergy.com/wp-content/uploads/2013/07/header_water-1024x403.jpg" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">WaterSense Products</h5>
-            <p class="card-text">Find approved WaterSense products you can use.</p>
+            <p class="card-text">WaterSense makes it simple to locate and choose water-saving products that benefit both your budget and the environment. Simply check for WaterSense-labeled goods at your local shop. Reduce your water use while enjoying great performance with the WaterSense branded goods listed below. To begin, click on the link below and select the types of products you want.</p>
             <a href="https://lookforwatersense.epa.gov/products/" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
@@ -44,7 +44,7 @@
             <img src="https://bellevuewa.gov/sites/default/files/styles/max_650x650/public/inline-images/2020-11/2021-2022%20typical%20bill%20graphic%20horizontal.PNG?itok=JgZqfxj9" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">Water Bills by State</h5>
-            <p class="card-text">How what state you live in affects your water bill.</p>
+            <p class="card-text">How what state you live in affects your water bill. Each drink of water, shower, or flush now costs significantly more than it did only eight years ago, and your water bill is expected to rise again in 2023. The average monthly water bill in the United States is $70.93 and the average American uses around 100 gallons of water each day.</p>
             <a href="https://dripfina.com/water-bill-by-us-states/#:~:text=What%27s%20The%20Average%20Monthly%20Water%20Bill%20in%20the,7%20Colorado%20...%208%20Connecticut%20...%20More%20items" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
@@ -55,7 +55,7 @@
             <img src="https://d9-wret.s3.us-west-2.amazonaws.com/assets/palladium/production/s3fs-public/styles/full_width/public/thumbnails/image/DO-trend.png?itok=xIMjHmdv" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">Domestic Water Usage Survey</h5>
-            <p class="card-text">Learn how water use changed over 65 years.</p>
+            <p class="card-text">Learn how water use changed over 65 years. Domestic water consumption encompasses both indoor and outdoor uses at homes, including drinking, food preparation, bathing, washing clothing and dishes, flushing toilets, watering lawns and gardens, and pool maintenance.</p>
             <a href="https://www.usgs.gov/mission-areas/water-resources/science/domestic-water-use" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
@@ -66,7 +66,7 @@
             <img src="https://www.conservenorthtexas.org/getmedia/0e6eb510-27fa-441c-8358-0e1547b85a87/EPA_USgpcd.png" class="card-img-top" alt="Water Bill Cover"/>
             <div class="card-body">
             <h5 class="card-title">Domestic Water Usage Growth</h5>
-            <p class="card-text">Learn about the project change in Water Use for 2030.</p>
+            <p class="card-text">Learn about the project change in Water Use for 2030. Many of the states with predicted population expansion have greater per capita water demand and will face increased competition for water resources. Communities around the country are beginning to face water supply difficulties, as well as the need to repair old water treatment and distribution systems, referred to as "water infrastructure."</p>
             <a href="https://www.conservenorthtexas.org/regional-indicators/us-domestic-water-use-gallons-day-person" target="_blank" class="btn btn-primary">Learn more</a>
             </div>
         </div>
